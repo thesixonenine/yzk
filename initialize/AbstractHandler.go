b17eafbf9872362be12a2f180ccbbaa5c68e8b2d
Simple <huyang1994822@gmail.com>
2022-11-30 23:51:47 +0800

    props

---
+++ initialize/AbstractHandler.go
@@ -0,0 +1,76 @@
+package initialize
+
+import (
+	"github.com/tencent-connect/botgo/log"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type absHandler interface {
+	handle()
+}
+type Props struct {
+	AppID int    `yml:"appid"`
+	Token string `yml:"token"`
+}
+
+func (p Props) check() {
+	if p.AppID == 0 || p.Token == "" {
+		log.Error("程序配置初始化失败")
+		os.Exit(1)
+	}
+}
+
+type envHandler struct {
+	props *Props
+}
+
+func (handler envHandler) handle() {
+	appIdEnvStr := "bot.appId"
+	tokenEnvStr := "bot.token"
+	appId := strings.TrimSpace(os.Getenv(appIdEnvStr))
+	if appId == "" {
+		log.Debugf("环境变量{%s}中没有appId", appIdEnvStr)
+	} else {
+		handler.props.AppID, _ = strconv.Atoi(appId)
+	}
+	token := strings.TrimSpace(os.Getenv(tokenEnvStr))
+	if token == "" {
+		log.Debugf("环境变量{%s}中没有token", tokenEnvStr)
+	} else {
+		handler.props.Token = token
+	}
+
+}
+
+type ymlHandler struct {
+	props *Props
+}
+
+func (handler ymlHandler) handle() {
+	fileName := "config.yml"
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Debug("读取配置文件出错, " + err.Error())
+		return
+	}
+	err = yaml.Unmarshal(content, handler.props)
+	if err != nil {
+		log.Debug("解析配置文件出错, ", err)
+	}
+}
+
+func Init() Props {
+	var prop Props
+	p := []absHandler{envHandler{&prop}, ymlHandler{&prop}}
+	log.Debug("开始初始化配置")
+	for _, handler := range p {
+		handler.handle()
+	}
+	log.Debug("结束初始化配置")
+	prop.check()
+	return prop
+}
--- robot.go
+++ robot.go
@@ -1,74 +1,71 @@
 package main
 
 import (
-    "context"
-    "io/ioutil"
-    "log"
-    "os"
-    "strings"
-    "time"
+	"context"
+	"log"
+	"strings"
+	"time"
+	"yzk/initialize"
 
-    "github.com/tencent-connect/botgo"
-    "github.com/tencent-connect/botgo/dto"
-    "github.com/tencent-connect/botgo/openapi"
-    "github.com/tencent-connect/botgo/token"
-    "github.com/tencent-connect/botgo/websocket"
-    "github.com/tencent-connect/botgo/event"
-    yaml "gopkg.in/yaml.v2"
+	"github.com/tencent-connect/botgo"
+	"github.com/tencent-connect/botgo/dto"
+	"github.com/tencent-connect/botgo/event"
+	"github.com/tencent-connect/botgo/openapi"
+	"github.com/tencent-connect/botgo/token"
+	"github.com/tencent-connect/botgo/websocket"
 )
 
 //Config 定义了配置文件的结构
-type Config struct {
-    AppID uint64 `yml:"appid"` //机器人的appid
-    Token string `yml:"token"` //机器人的token
-}
+//type Config struct {
+//	AppID uint64 `yml:"appid"` //机器人的appid
+//	Token string `yml:"token"` //机器人的token
+//}
 
-var config Config
+var config initialize.Props
 var api openapi.OpenAPI
 var ctx context.Context
 
-//第一步： 获取机器人的配置信息，即机器人的appid和token
+// 获取机器人的配置信息，即机器人的appid和token
 func init() {
-    content, err := ioutil.ReadFile("config.yml")
-    if err != nil {
-        log.Println("读取配置文件出错， err = ", err)
-        os.Exit(1)
-    }
-
-    err = yaml.Unmarshal(content, &config)
-    if err != nil {
-        log.Println("解析配置文件出错， err = ", err)
-        os.Exit(1)
-    }
-    log.Println(config)
+	props := initialize.Init()
+	log.Println(props)
 }
 
-//atMessageEventHandler 处理 @机器人 的消息
+// 处理 @机器人 的消息
 func atMessageEventHandler(event *dto.WSPayload, data *dto.WSATMessageData) error {
-    if strings.HasSuffix(data.Content, "> hello") { // 如果@机器人并输入 hello 则回复 你好。
-        api.PostMessage(ctx, data.ChannelID, &dto.MessageToCreate{MsgID: data.ID, Content: "这里没有涩图可以看"})
-    } else {
-        api.PostMessage(ctx, data.ChannelID, &dto.MessageToCreate{MsgID: data.ID, Content: "这是默认回复"})
+	if strings.HasSuffix(data.Content, "> hello") { // 如果@机器人并输入 hello 则回复 你好。
+		_, err := api.PostMessage(ctx, data.ChannelID, &dto.MessageToCreate{MsgID: data.ID, Content: "这里没有涩图可以看"})
+		if err != nil {
+			return nil
+		}
+	} else {
+		_, err := api.PostMessage(ctx, data.ChannelID, &dto.MessageToCreate{MsgID: data.ID, Content: "这是默认回复"})
+		if err != nil {
+			return nil
+		}
 	}
-    return nil
+	return nil
 }
 
 func main() {
-    //第二步：生成token，用于校验机器人的身份信息
-    token := token.BotToken(config.AppID, config.Token) 
-    //第三步：获取操作机器人的API对象
-    api = botgo.NewOpenAPI(token).WithTimeout(3 * time.Second)
-    //获取context
-    ctx = context.Background()
-    //第四步：获取websocket
-    ws, err := api.WS(ctx, nil, "") 
-    if err != nil {
-        log.Fatalln("websocket错误， err = ", err)
-        os.Exit(1)
-    }
+	//第二步：生成token，用于校验机器人的身份信息
+	botToken := token.BotToken(uint64(config.AppID), config.Token)
+	//第三步：获取操作机器人的API对象
+	api = botgo.NewOpenAPI(botToken).WithTimeout(3 * time.Second)
+	//获取context
+	ctx = context.Background()
+	//第四步：获取websocket
+	ws, err := api.WS(ctx, nil, "")
+	if err != nil {
+		log.Fatalln("websocket错误， err = ", err)
+	}
 
-    var atMessage event.ATMessageEventHandler = atMessageEventHandler
+	var atMessage event.ATMessageEventHandler = atMessageEventHandler
 
-    intent := websocket.RegisterHandlers(atMessage)     // 注册socket消息处理
-    botgo.NewSessionManager().Start(ws, token, &intent) // 启动socket监听
+	// 注册socket消息处理
+	intent := websocket.RegisterHandlers(atMessage)
+	err = botgo.NewSessionManager().Start(ws, botToken, &intent)
+	if err != nil {
+		return
+	} // 启动socket监听
 }
