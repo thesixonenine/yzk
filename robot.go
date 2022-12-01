eca866cdd5e67c90fbe1ed4046ed50ce10d0a871
Simple <huyang1994822@gmail.com>
2022-12-01 11:10:32 +0800

    update

--- robot.go
+++ robot.go
@@ -18,14 +18,12 @@ var botConfig config.Config
 var api openapi.OpenAPI
 var ctx context.Context
 
-// 获取机器人的配置信息，即机器人的appId和token
 func init() {
 	config.Init(&botConfig)
 }
 
-// 处理 @机器人 的消息
 func atMessageEventHandler(event *dto.WSPayload, data *dto.WSATMessageData) error {
-	if strings.HasSuffix(data.Content, "> hello") { // 如果@机器人并输入 hello 则回复 你好。
+	if strings.HasSuffix(data.Content, "> hello") {
 		_, err := api.PostMessage(ctx, data.ChannelID, &dto.MessageToCreate{MsgID: data.ID, Content: "这里没有涩图可以看"})
 		if err != nil {
 			return nil
@@ -40,13 +38,9 @@ func atMessageEventHandler(event *dto.WSPayload, data *dto.WSATMessageData) erro
 }
 
 func main() {
-	//第二步：生成token，用于校验机器人的身份信息
 	botToken := token.BotToken(uint64(botConfig.BotAppId), botConfig.BotToken)
-	//第三步：获取操作机器人的API对象
 	api = botgo.NewOpenAPI(botToken).WithTimeout(3 * time.Second)
-	//获取context
 	ctx = context.Background()
-	//第四步：获取websocket
 	ws, err := api.WS(ctx, nil, "")
 	if err != nil {
 		log.Fatalln("websocket错误， err = ", err)
@@ -54,10 +48,9 @@ func main() {
 
 	var atMessage event.ATMessageEventHandler = atMessageEventHandler
 
-	// 注册socket消息处理
 	intent := websocket.RegisterHandlers(atMessage)
 	err = botgo.NewSessionManager().Start(ws, botToken, &intent)
 	if err != nil {
 		return
-	} // 启动socket监听
+	}
 }
